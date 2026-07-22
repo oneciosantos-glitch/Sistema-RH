6262c5016163a1fadf15a5e36f142166cb16fa8c
oneciosantos-glitch <oneciosantos@gmail.com>
2026-07-22 12:01:19 -0300

    Update app_rh.py

--- app_rh.py
+++ app_rh.py
@@ -363,7 +363,7 @@ with aba1:
             dt_adm = datetime.strptime(val_campo("Admissao"), "%d/%m/%Y")
             hoje = datetime.now()
             dias_corridos = (hoje - dt_adm).days
-            for prazo in [30, 45, 60, 90]:
+            for prazo in [29, 44, 59, 89]:
                 rest = prazo - dias_corridos
                 if rest > 0:
                     status = f"Faltam {rest} dias"
