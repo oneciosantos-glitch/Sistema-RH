80d4f8a08389e4ac1a0e03a5a79f7696ce1e6212
oneciosantos-glitch <oneciosantos@gmail.com>
2026-08-05 23:48:18 -0300

    Delete INICIAR_SISTEMA.bat

--- INICIAR_SISTEMA.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title CADASTRO DE FUNCIONÁRIOS
-echo Iniciando o Sistema...
-echo ==============================================
-python -m streamlit run app_rh.py
-echo ==============================================
-pause
